ca341172980a830fa2a3c3afd5295a98dec05f7a
Utsab-2010 <utsabkaran2010@gmail.com>
2025-10-14 15:47:56 +0530

    update

--- OpenMPTut1/OpenmpCodes/C/5threadprivate.c
+++ OpenMPTut1/OpenmpCodes/C/5threadprivate.c
@@ -4,21 +4,21 @@
   
 int main(int argc, char* argv[]) 
 {
-int id;
-static int x; 
-#pragma omp threadprivate(x)
-#pragma omp parallel private(id)
-{
-id=omp_get_thread_num();
-x = x+id; // updating threadprivate x
-printf("private: x is %d from thread: %d\n",x,id); 
-} 
-printf("after first parallel loop: x is %d\n",x);
+    int id;
+    static int x; 
+    #pragma omp threadprivate(x)
+    #pragma omp parallel private(id)
+    {
+        id = omp_get_thread_num();
+        x = x+id; // updating threadprivate x
+        printf("private: x is %d from thread: %d\n",x,id); 
+    } 
+    printf("after first parallel loop: x is %d\n",x);
 
-#pragma omp parallel private(id) 
-{
-id=omp_get_thread_num();
-x = x+10; // updating threadprivate x
-printf("private: x is %d from thread: %d\n",x,id);
-} // writing threadprivate variables 
+    #pragma omp parallel private(id) 
+    {
+        id = omp_get_thread_num();
+        x = x+10; // updating threadprivate x
+        printf("private: x is %d from thread: %d\n",x,id);
+    } // writing threadprivate variables 
 } // second parallel loop 
